934016e1478814ff37acf22154b51f6e27d457a3
mira48 <inioluwamayrih48@gmail.com>
2024-04-19 11:20:26 +0100

    logistics

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+// const navBar = document.getElementsByClassName("nav__bar")
+
+// if(window.scrollY >= 100){
+//     navBar.style.backgroundColor = "white";
+// }
+// else {
+//     // navBar.style.backgroundColor = "transparent";
+// }
